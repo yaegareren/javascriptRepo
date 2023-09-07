f29e5f8a1f283fdea088ac26930b579bbfd6fa90
Shree Mangalam Singh <sshreemangalam@gmail.com>
2023-09-07 15:54:33 +0000

    added variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 12345 // const cannot be changed 
+let accountEmail = "shreemangalam@48gmail.com" //  can be changed 
+var accPassword = "shree0102"
+let accountCity = "Ranchi"
+
+console.log([accountId, accountEmail, accPassword, accountCity])
