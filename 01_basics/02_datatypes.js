5e72cf04e700822a005173f38c8343385692cb0c
Shree Mangalam Singh <sshreemangalam@gmail.com>
2023-09-07 16:12:43 +0000

    added datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,21 @@
 "use strict"; // treat all js code as newer version of js 
+
+let name = "Shree Mangalam"
+let age = 18
+let isLoggedIn = false
+
+// primitive data types 
+// number -> 2^53
+// bigint -> for large numerical values 
+// string -> '' or ""
+
+// boolean -> true or false 
+// undefined -> 
+// null -> standalone value ; example let state = null
+// symbol -> unique
+
+
+// object 
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
