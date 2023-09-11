0bb95282588def9ea78b9f8abef0992124496c59
Shree Mangalam Singh <sshreemangalam@gmail.com>
2023-09-11 13:19:59 +0000

    added datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,51 @@
+// Javascript is a dynamically typed language. We do not define datatypes while declaring variables
+/*
+PRIMITIVE 
+they are of 7 types
+String 
+Number 
+Boolean 
+Null
+Undefined 
+BigInt 
+Symbol - used to make values unique
+
+*/
+const score = 100
+const scoreValue = 100.3 // this is also categorized as number 
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // undefined 
+
+// DEFINING SYMBOL
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); // false because symbol makes them unique
+
+const bigNumber = 123456785039345n // bigInt
+
+
+/*
+REFERENCE (NON PRIMITIVE)
+Arrays
+Objects
+Functions 
+*/
+
+const heroes = ["ironMan", "batMan", "spiderMan"] // array
+
+let myObject = {
+    name: "ShreeMangalam", 
+    age: 21, 
+}
+
+// function 
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof scoreValue) // Number 
+console.log(typeof myFunction) // Function 
