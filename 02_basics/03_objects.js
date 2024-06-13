3160f1130e33007a72f3bac6a76fefb90b1c28d0
Shree Mangalam Singh <sshreemangalam@gmail.com>
2024-06-13 16:02:13 +0000

    added objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,32 +1,31 @@
 // singleton - an object that is made through constructor
-// Object.create can be used to create an object, a singleton is formed 
+// Object.create can be used to create an object, a singleton is formed
 
-// object literals 
-// defining objects 
+// object literals
+// defining objects
 
-const mySym = Symbol("key1")
+const mySym = Symbol("key1");
 const JsUser = {
-    name: "Mangalam", 
-    "full name":  "Shree Mangalam Singh", 
-    age: 21, 
-   [mySym]: "mykey1", 
-    location: "Ranchi", 
-    email: "shreemangalam48@gmail.com", 
-    isLoggedIn: false, 
-    lastLoggedinDays: ["Monday", "Saturday"]
+  name: "Mangalam",
+  "full name": "Shree Mangalam Singh",
+  age: 21,
+  [mySym]: "mykey1",
+  location: "Ranchi",
+  email: "shreemangalam48@gmail.com",
+  isLoggedIn: false,
+  lastLoggedinDays: ["Monday", "Saturday"],
+};
 
-}
-
-// accessing objects 
+// accessing objects
 // console.log(JsUser.email)
 // console.log(JsUser["email"])
 
 // using square notation is a better option
-console.log(JsUser["full name"])
-console.log(JsUser[mySym]) // symbol printing
+console.log(JsUser["full name"]);
+console.log(JsUser[mySym]); // symbol printing
 
-JsUser.email = "shreemangalam29@gmail.com"
-console.log(JsUser["email"])
+JsUser.email = "shreemangalam29@gmail.com";
+console.log(JsUser["email"]);
 
-Object.freeze(JsUser) // values cannot be changed once freeze method is applied 
+Object.freeze(JsUser); // values cannot be changed once freeze method is applied
 console.log(JsUser);
--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,2 +1,43 @@
-const tinderUser = new Object(); 
-console.log(tinderUser)
+// const tinderUser = {} ; // non-singleton object
+const tinderUser = new Object(); // singleton object
+
+tinderUser.id = "abc123";
+tinderUser.name = "Phalak";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "some@gmail.com",
+  fullname: {
+    firstname: "Phalak",
+    lastname: "Taneja",
+  },
+};
+
+// console.log(regularUser.fullname.lastname);
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+
+// const obj3 = {obj1, obj2} // object inside object
+const obj3 = Object.assign({}, obj1, obj2); // = {...obj1, ...obj2}
+console.log(obj3);
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn")); // true
+
+// output in form of an array
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+
+const course = {
+  coursename: "js-classes",
+  price: "999",
+  courseInstructor: "hitesh",
+};
+
+// course.courseInstructor
+
+const { courseInstructor: instructor } = course;
+
+console.log(instructor);
